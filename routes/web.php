72dccde1a8dc3db92067d23e2d18e2d16e83e7e4
SEKOUSONOMOU <sekou.sonomou@techyao.com>
2024-10-24 17:48:56 +0000

    20241024

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
 /*
@@ -19,11 +19,11 @@ use App\Http\Controllers\DashboardController;
 //     return view('welcome');
 // });
 
-Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('register', [AuthController::class, 'register']);
-Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('login', [AuthController::class, 'login']);
-Route::post('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
+// Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
+// Route::post('register', [AuthController::class, 'register']);
+// Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+// Route::post('login', [AuthController::class, 'login']);
+// Route::post('logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 
